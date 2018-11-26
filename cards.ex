f4f1c3e1ffd39adc85e6300d19e764a07a66d61e
Anna Grushevskaya <45317861+annagrushe@users.noreply.github.com>
2018-11-26 22:32:31 +0300

    Add files via upload

--- cards.ex
+++ cards.ex
@@ -1,12 +1,12 @@
 defmodule Cards do
 
  def create_deck do
-  values = ["Ace","Two","Three","Four","Five"]
-  suits = ["Spades","Clubs","Hearts","Diamonds"]
+   values = ["Ace","Two","Three","Four","Five"]
+   suits = ["Spades","Clubs","Hearts","Diamonds"]
 
- for suit <- suits, value <- values do
-   "#{value} of #{suit}"
- end
+   for suit <- suits, value <- values do
+     "#{value} of #{suit}"
+   end
  end
 
  def shuffle(deck) do
@@ -21,9 +21,20 @@ defmodule Cards do
   Enum.split(deck, hand_size)
  end
 
-def save(deck, filename) do
- binary=:erlang.term_to_binary(deck)
- File.write(filename,binary)
+ def save(deck, filename) do
+  binary=:erlang.term_to_binary(deck)
+  File.write(filename,binary)
+ end
+
+ def load(filename) do
+  case File.read(filename) do
+    {:ok, binary} -> :erlang.binary_to_term binary
+    {:error, _reason} -> "That file does not exist"
+  end
+
+ end
+
+def create_hand(hand_size) do
 
 end
 
