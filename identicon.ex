e67883dbe9906dc75992d52cf55640b950cdaa36
Anna Grushevskaya <annita007@mail.ru>
2018-11-29 22:24:02 +0300

    Update identicon.ex

--- identicon.ex
+++ identicon.ex
@@ -7,11 +7,33 @@ defmodule Identicon do
   def main(string) do
     string
     |> hashing
+    |> pick_color
+    |> build_grid
   end
 
-  def hashing (string) do
-    :crypto.hash(:md5, string)
-    |> :binary.bin_to_list
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+   hex_list
+   |> Enum.chunk(3)
+   |> Enum.map(&mirror_row/1)
   end
+
+  def mirror_row([a, b, c] = row) do
+    #[a, b, c, b, a] or:
+    [a, b | _tail] = row
+    row ++ [a, b]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hashing(string) do
+    hex = :crypto.hash(:md5, string)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+
 
 end
